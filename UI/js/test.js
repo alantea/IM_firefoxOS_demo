23ca9c35df87a523778230a3bbe9bee645e4f526
veronica82915 <veronica82915@gmail.com>
2013-07-15 18:41:54 +0800

    main page 架構finish

--- UI/js/test.js
+++ UI/js/test.js
@@ -13,5 +13,10 @@ $(document).ready(function(){
    		$('#loginpage').removeClass('slide-up-in');
    		$('#loginpage').addClass('slide-up-in');
 	});
-	
+
+	$('#friend_link').click(function(){
+		$('#friendpage').attr('aria-hidden', 'false');
+   		$('#mainpage').attr('aria-hidden', 'true');
+   		$('#mainpage').addClass('slide-out-down');
+	});
 });
