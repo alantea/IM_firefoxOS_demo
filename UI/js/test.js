74504b5c927baae4d71b44e4bc1c55ee28612581
veronica82915 <veronica82915@gmail.com>
2013-07-25 13:01:03 +0800

    chat page submit

--- UI/js/test.js
+++ UI/js/test.js
@@ -1,22 +1,40 @@
 $(document).ready(function(){
+	
 	$('#unregister').click(function(){
 		$('#registerpage').attr('aria-hidden', 'false');
    		$('#loginpage').attr('aria-hidden', 'true');
-   		$('#registerpage').removeClass('slide-up-in');
    		$('#registerpage').addClass('slide-up-in');
-   		
+   		$('#loginpage').addClass('slide-down-out');
+   		$('#registerpage').removeClass('slide-down-out');
+   		$('#loginpage').removeClass('slide-up-in');
 	});
 
 	$('.icon-back').click(function(){
 		$('#registerpage').attr('aria-hidden', 'true');
    		$('#loginpage').attr('aria-hidden', 'false');
-   		$('#loginpage').removeClass('slide-up-in');
+   		$('#loginpage').removeClass('slide-down-out');
+   		$('#registerpage').removeClass('slide-up-in');
+   		$('#registerpage').addClass('slide-down-out');
    		$('#loginpage').addClass('slide-up-in');
 	});
 
 	$('#friend_link').click(function(){
 		$('#friendpage').attr('aria-hidden', 'false');
    		$('#mainpage').attr('aria-hidden', 'true');
-   		$('#mainpage').addClass('slide-out-down');
+   		$('#mainpage').addClass('slide-up-in');
 	});
+
+	$('#addfriend').click(function(){
+		$('#friendpage').attr('aria-hidden', 'true');
+		$('#addpage').attr('aria-hidden', 'false');
+		$('#friendpage').addClass('slide-down-out');
+		$('#addpage').addClass('slide-up-in');
+	});
+
+	$('#send').click(function(){
+		text = $("textarea[name='userchat']").val();
+		$("#chatcontact").append("<div class=\"chat\">" + text + "</div>");
+		$("textarea[name='userchat']").val("");
+	});
+    
 });
